17dd94ad635e5b8b7c738a3ec576862d27e0c778
aimless-cpu <s.grabher@protonmail.com>
2021-04-21 13:21:14 +0200

    added Method fillPlayField

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+    public static Scanner sc = new Scanner(System.in);
+
     public static final int ROWS = 6;
     public static final int COLUMNS = 7;
     static char[][] playField = new char[COLUMNS][ROWS];
@@ -10,8 +13,16 @@ public class Main {
     public static void main(String[] args) {runGame();}
     public static void runGame() {
 
-        char playFieldFiller = '*';
-        fillPlayField(playFieldFiller);
+        char playFieldEmptySlot = '*';
+        fillPlayField(playFieldEmptySlot);
+
+        //boolean isPlayer1 = true;
+
+        while (true) {
+
+            int userInput = sc.nextInt();
+
+        }
 
 
     }
